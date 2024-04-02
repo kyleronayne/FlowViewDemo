0297cec978c50112ce81c250d46f453831513a93
Kyle Ronayne <kyle.ronayne@icloud.com>
2024-04-02 12:07:19 -0400

    Initial Commit

---
+++ FlowViewDemo/FlowViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowViewDemoApp.swift
+//  FlowViewDemo
+//
+//  Created by Kyle Ronayne on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct FlowViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
